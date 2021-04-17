61694e47fdc6df85886cb9a89512dcee5cba225c
Toooble <tobiclement83@gmail.com>
2021-04-17 15:50:49 +0100

    Proto main menu added
    Text mesh pro added

---
+++ DeliveryGameNew/Assets/Scripts/Menus/Main/MainmenuScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainmenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
